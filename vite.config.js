88faa262ca03a005ee8b727826535c6bb27ae953
yeyongzhi <18258475212m@sina.cn>
2025-12-19 13:50:04 +0800

    feat: 更新路由根地址

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-const repoName = 'oversea_shop'
-
 // https://vite.dev/config/
 export default defineConfig({
-  base: `/${repoName}/`,
+  base: process.env.VITE_BASE_PATH || '/', // 默认为根路径
   plugins: [vue()],
 })
