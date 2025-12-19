418208f528fa4fe94e213df271aa351006d08ddb
yeyongzhi <18258475212m@sina.cn>
2025-12-19 10:45:47 +0800

    feat: 补充deploy文件 1.完善路由

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const repoName = 'oversea_shop'
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: `/${repoName}/`,
   plugins: [vue()],
 })
